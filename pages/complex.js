d910dcbfe6bb4ca6ce7070f0fcb27cd10f92a4cc
Ferdinand Prantl <prantlf@gmail.com>
2019-03-01 18:06:20 +0100

    Remove unused functions and fix linting warnings

--- pages/complex.js
+++ pages/complex.js
@@ -1,34 +1,21 @@
 // Operations on complex numbers. A complex number is represented
 // by an object with real and imaginary parts as keys: { r, i }.
 
-function equal ({ r: r1, i: i1 }, { r: r2, i: i2 }) {
-  return r1 === r2 && i1 === i2
-}
-
+// eslint-disable-next-line no-unused-vars
 function add ({ r: r1, i: i1 }, { r: r2, i: i2 }) {
   const r = r1 + r2
   const i = i1 + i2
   return { r, i }
 }
 
-function multiply ({ r: r1, i: i1 }, { r: r2, i: i2 }) {
-  const r = r1 * r2 - i1 * i2
-  const i = r1 * i2 + i1 * r2
-  return { r, i }
-}
-
+// eslint-disable-next-line no-unused-vars
 function power2 ({ r: r1, i: i1 }) {
   const r = r1 * r1 - i1 * i1
   const i = 2 * r1 * i1
   return { r, i }
 }
 
-function absoluteValue ({ r, i }) {
-  return Math.sqrt(r * r + i * i)
-}
-
+// eslint-disable-next-line no-unused-vars
 function absoluteValue2 ({ r, i }) {
   return r * r + i * i
 }
-
-// export { equal, add, multiply, power2, absoluteValue, absoluteValue2 }
